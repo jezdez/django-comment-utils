ab2aa847346ce7d1e82f7691d0f024b89452d086
ubernostrum <ubernostrum@71c79c79-8932-0410-8db2-b50025d4c983>
2007-06-18 04:19:26 +0000

    Another
    git-svn-id: http://django-comment-utils.googlecode.com/svn/trunk@31 71c79c79-8932-0410-8db2-b50025d4c983

--- comment_utils/moderation.py
+++ comment_utils/moderation.py
@@ -276,7 +276,7 @@ class CommentModerator(object):
         if instance.id or (instance.content_type_id not in self._registry)
             return
         content_object = instance.get_content_object()
-        moderation_class = self._registry(instance.content_type_id)
+        moderation_class = self._registry[instance.content_type_id]
         if not moderation_class.allow(instance, content_object): # Comment will get deleted in post-save hook.
             instance.moderation_disallowed = True
             return
@@ -295,6 +295,7 @@ class CommentModerator(object):
         if instance.moderation_disallowed:
             instance.delete()
             return
+        moderation_class = self._registry[instance.content_type_id]
         moderation_class.email(instance, content_object)
 
 
