7a987dda510b6ee70bb8a621f9942dcea9179643
ubernostrum <ubernostrum@71c79c79-8932-0410-8db2-b50025d4c983>
2007-06-22 06:35:00 +0000

    Stub out docs and packaging
    git-svn-id: http://django-comment-utils.googlecode.com/svn/trunk@56 71c79c79-8932-0410-8db2-b50025d4c983

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='comment_utils',
+      version='0.1',
+      description='Comment-related utilities for Django applications',
+      author='James Bennett',
+      author_email='james@b-list.org',
+      url='http://code.google.com/p/django-comment-utils/',
+      packages=['comment_utils', 'comment_utils.templatetags'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
